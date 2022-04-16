be5028c83a6b318368f2d55f8ea79e7134dcc917
Alfaza Satria <alfazasatria8@gmail.com>
2022-04-17 00:27:34 +0700

    initial Commit

--- cypress/integration/sauce.spec.js
+++ cypress/integration/sauce.spec.js
@@ -1,7 +1,30 @@
 
-describe('sauce', () => {
+import { CommonPage, LoginPage } from '../support/pages';
+
+context('Login', () => {
     beforeEach(() => {
-      
       cy.visit('https://www.saucedemo.com/')
     })
+
+    describe('User', () => {
+        it('can login with standard user', () => {
+          LoginPage.UserName()
+            .type('standard_user')
+            .should('have.value', 'standard_user')
+    
+          LoginPage.Password()
+            .type('secret_sauce')
+            .should('have.value', 'secret_sauce')
+    
+          LoginPage.LoginButton().click()
+    
+        })
+
+        
+        
+      })
+
+    
+
 })
+
